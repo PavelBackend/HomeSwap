c4626abd42ffc5d5340b9f410b0d24177d1ab179
Pavel <samoesamokhin@yandex.ru>
2024-10-13 00:14:05 +0300

    Docker-compose updated

--- main_hms/main_hms/settings.py
+++ main_hms/main_hms/settings.py
@@ -74,10 +74,21 @@ TEMPLATES = [
 
 # WSGI_APPLICATION = "main_hms.wsgi.application"
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home_swap_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
